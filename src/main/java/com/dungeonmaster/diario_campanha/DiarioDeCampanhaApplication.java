c967e253292bdc4a434cc21a4777797b2dae7779
jchrisostomo <joao.chrisostomo@gmail.com>
2024-11-11 23:23:48 -0300

    Projeto criado

---
+++ src/main/java/com/dungeonmaster/diario_campanha/DiarioDeCampanhaApplication.java
@@ -0,0 +1,13 @@
+package com.dungeonmaster.diario_campanha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarioDeCampanhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiarioDeCampanhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dungeonmaster/diario_campanha/DiarioDeCampanhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dungeonmaster.diario_campanha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiarioDeCampanhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
